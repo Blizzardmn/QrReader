1ae4f02f7fa9d34901d35171232d542b4528375e
Gasol <315710785@qq.com>
2023-05-05 10:10:07 +0800

    add topOn sdk

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -21,3 +21,26 @@
 #-renamesourcefileattribute SourceFile
 -keep class androidx.** {*;}
 -keep class android.app.** {*;}
+
+#TopOn Keep Guard
+-keep public class com.anythink.**
+-keepclassmembers class com.anythink.** {
+   *;
+}
+
+-keep public class com.anythink.network.**
+-keepclassmembers class com.anythink.network.** {
+   public *;
+}
+
+-dontwarn com.anythink.hb.**
+-keep class com.anythink.hb.**{ *;}
+
+-dontwarn com.anythink.china.api.**
+-keep class com.anythink.china.api.**{ *;}
+
+# new in v5.6.6
+-keep class com.anythink.myoffer.ui.**{ *;}
+-keepclassmembers public class com.anythink.myoffer.ui.** {
+   public *;
+}
--- app/src/main/java/com/qr/myqr/App.kt
+++ app/src/main/java/com/qr/myqr/App.kt
@@ -5,6 +5,16 @@ import android.app.Application
 import android.content.Context
 import android.os.Build
 import android.os.Process
+import android.util.Log
+import android.webkit.WebView
+import com.anythink.core.api.ATInitConfig
+import com.anythink.core.api.ATNetworkConfig
+import com.anythink.core.api.ATSDK
+import com.anythink.core.api.DeviceInfoCallback
+import com.anythink.network.adcolony.AdColonyATInitConfig
+import com.anythink.network.mintegral.MintegralATInitConfig
+import com.anythink.network.pangle.PangleATInitConfig
+import com.anythink.network.vungle.VungleATInitConfig
 import com.qr.myqr.page.FirstActivity
 import com.reader.multiple.bmw4.MvpManager
 import com.reader.multiple.vb.MvpFbObj
@@ -19,10 +29,19 @@ class App : Application() {
         super.onCreate()
         mApp = this
 
+        //Android 9及以上必须设置 多进程WebView兼容
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val processName = getProcessName()
+            if (packageName != processName) {
+                WebView.setDataDirectorySuffix(processName)
+            }
+        }
         MvpManager.setBhs(this, true)
         MvpManager.init(this)
 
         if (processName() != packageName) return
+        initTopOn()
+
         if (isReleaseMode && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
             MvpFbObj.hi(this, FirstActivity::class.java.name)
         }
@@ -38,6 +57,42 @@ class App : Application() {
             }
         }
         return null
+    }
+
+    private val tapOnAppId = "a62b013be01931"
+    private val tapOnAppKey = "c3d0d2a9a9d451b07e62b509659f7c97"
+    private fun initTopOn() {
+        ATSDK.setNetworkLogDebug(!isReleaseMode) //SDK日志功能，集成测试阶段建议开启，上线前必须关闭
+        Log.i("", "TopOn SDK version: " + ATSDK.getSDKVersionName()) //SDK版本
+        ATSDK.integrationChecking(applicationContext) //检查广告平台的集成状态，提交审核时需注释此API
+        //(v5.7.77新增) 打印当前设备的设备信息(IMEI、OAID、GAID、AndroidID等)
+        ATSDK.testModeDeviceInfo(this, object : DeviceInfoCallback {
+            override fun deviceInfo(deviceInfo: String) {
+                Log.i("", "deviceInfo: $deviceInfo")
+            }
+        })
+
+        val atInitConfigs = ArrayList<ATInitConfig>()
+        //Mintegral
+        val mintegralATInitConfig = MintegralATInitConfig(tapOnAppId, tapOnAppKey)
+        //AdColony
+        val adColonyATInitConfig = AdColonyATInitConfig(tapOnAppId, "b62b037cc24451")
+        //Pangle
+        val pangleATInitConfig = PangleATInitConfig(tapOnAppId)
+        //Vungle
+        val vungleAtInitConfig = VungleATInitConfig(tapOnAppId)
+
+        atInitConfigs.add(pangleATInitConfig)
+        atInitConfigs.add(mintegralATInitConfig)
+        atInitConfigs.add(vungleAtInitConfig)
+        atInitConfigs.add(adColonyATInitConfig)
+
+        val builder = ATNetworkConfig.Builder()
+        builder.withInitConfigList(atInitConfigs)
+        val atNetworkConfig = builder.build()
+
+        //初始化TopOn SDK以及广告平台SDK
+        ATSDK.init(applicationContext, tapOnAppId, tapOnAppKey, atNetworkConfig)
     }
 
 }
